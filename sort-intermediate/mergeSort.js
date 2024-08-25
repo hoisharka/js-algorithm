67939c57930073268f6064b802c9869ac5f9405c
hoisharka <hoisharka@gmail.com>
2024-08-25 21:35:10 +0900

    merge sort

--- sort-intermediate/mergeSort.js
+++ sort-intermediate/mergeSort.js
@@ -9,7 +9,7 @@ function mergeSort(arr) {
     if (arr.length < 2) {
         return arr
     }
-
+ 
     const mid = Math.floor(arr.length / 2)
     const left = mergeSort(arr.slice(0, mid))
     const right = mergeSort(arr.slice(mid))
